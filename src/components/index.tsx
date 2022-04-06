6b1b874f399121f0fa685505e1edd108846aaa11
puruhitaaa <hughdev101@gmail.com>
2022-04-06 19:20:47 +0700

    [+] Added custom shared Button component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,5 @@ export { default as Header } from './Header';
 export { default as FeedbackItem } from './FeedbackItem';
 export { default as FeedbackList } from './FeedbackList';
 export { default as Card } from './shared/Card';
+export { default as Button } from './shared/Button';
 export { default as FeedbackStats } from './FeedbackStats';
---
+++ src/components/shared/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode;
+  isDisabled: boolean;
+  type: 'button' | 'submit';
+  variant: string;
+}
+
+const Button = ({
+  children,
+  isDisabled = false,
+  type = 'button',
+  variant = 'primary',
+}: ButtonProps) => {
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${variant}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
